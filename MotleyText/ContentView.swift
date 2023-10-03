89bfa51c4818bdf813e3def20a0933a7838ea7f1
mrdemqn <mrdemqnvip@gmail.com>
2023-10-03 16:46:16 +0300

    Initial Commit

---
+++ MotleyText/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  MotleyText
+//
+//  Created by Димон on 3.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MotleyText/MotleyTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotleyTextApp.swift
+//  MotleyText
+//
+//  Created by Димон on 3.10.23.
+//
+
+import SwiftUI
+
+@main
+struct MotleyTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
